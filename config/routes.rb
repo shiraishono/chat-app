2a1bedbc55ff187c8b80a9ced44f66bc8feca9fb
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-02-10 00:06:27 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
-  root to: "messages#index"
+  root to: 'messages#index'
 end
